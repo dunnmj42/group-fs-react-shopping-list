1cf22144a82a9204568de5e7c09adff5b8f1adf8
Michael Dunn <dunnmj42@gmail.com>
2021-01-14 16:37:37 -0600

    item and shopping list style

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,35 +1,32 @@
-import Card from 'react-bootstrap/Card'
-import CardColumns from 'react-bootstrap/CardColumns'
-import Button from 'react-bootstrap/Button'
-import './Item.css'
-import RemoveItem from '../RemoveItem/RemoveItem'
+import Card from "react-bootstrap/Card";
+import CardColumns from "react-bootstrap/CardColumns";
+import Button from "react-bootstrap/Button";
+import "./Item.css";
+import RemoveItem from "../RemoveItem/RemoveItem";
 
-function Item({itemList, handleRemove}) {
-    
-    return (
-        <CardColumns>
-            {itemList.map(item => (
-                <Card 
-                    bg="light" 
-                    style={{ width: '18rem' }} 
-                    border="secondary" 
-                    className="p-3" 
-                    key={item.id}
-                >
-                    <Card.Body>
-                        <Card.Title>{item.item_name}</Card.Title>
-                        <Card.Text>
-                        {item.quantity} {item.unit}
-                        </Card.Text>
-                        <Button variant="primary">Buy</Button>
-                        <RemoveItem 
-                            handleRemove={handleRemove}
-                        />
-                    </Card.Body>
-                </Card>
-            ))}
-        </CardColumns>
-    )
+function Item({ itemList, handleRemove }) {
+  return (
+    <CardColumns>
+      {itemList.map((item) => (
+        <Card
+          bg="light"
+          style={{ width: "18rem", textAlign: "center" }}
+          border="secondary"
+          className="p-3"
+          key={item.id}
+        >
+          <Card.Body>
+            <Card.Title>{item.item_name}</Card.Title>
+            <Card.Text>
+              {item.quantity} {item.unit}
+            </Card.Text>
+            <Button variant="primary">Buy</Button>
+            <RemoveItem handleRemove={handleRemove} />
+          </Card.Body>
+        </Card>
+      ))}
+    </CardColumns>
+  );
 }
 
 export default Item;
