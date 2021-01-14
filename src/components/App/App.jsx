5b541de5484c321c9e7cd42e00dc243b93496d9e
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2021-01-14 14:07:40 -0600

    Delete moved into a new commponent.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,27 +2,28 @@ import React from 'react';
 import axios from 'axios';
 import Header from '../Header/Header.jsx'
 import './App.css';
+import RemoveItem from '../RemoveItem/RemoveItem'
 
 
 function App() {
 
-    const removeItem =()=> {
-        console.log('delete clicked');
 
-        let id = 3;
-        axios.delete(`/list/${id}`).then((response) => {
-            console.log(response);
-        }).catch((err) => {
-            console.log(err);
-        })
+    const handelRemove = (event) => {
+        event.preventDefault();
+        console.log('Delete clicked');
     }
+    
     return (
         <div className="App">
             <Header />
             <main>
                 <p>Under Construction...</p>
             </main>
-            <button onClick={removeItem}>Delete</button>
+            <RemoveItem 
+            handelRemove={handelRemove}
+            
+            />
+            
         </div>
     );
 }
