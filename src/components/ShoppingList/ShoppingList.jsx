6a6d8ae9988ec234c8f8815273955575d43b485d
Gabin Williams <gabinwilliams@gmail.com>
2021-01-14 13:29:45 -0600

    working on buttons in ShoppingList feature

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,26 +1,31 @@
 import {useState} from 'react';
 import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+
+const element = <FontAwesomeIcon icon={faTrashAlt} />
 
 
-function ShoppingList() {
+
+
+function ShoppingList({removeItem}) {
 
   return (
-
 
   <div>
     <h2>Shopping List</h2>
     <hr/>
     <div>
-      <Button variant="primary" size="sm">
-        Small button
+      <Button variant="danger" size="sm">{element}
+        
       </Button>{' '}
-      <Button variant="secondary" size="sm">
-        Small button
+      <Button variant="danger" size="sm">
+        Clear
       </Button>
     </div>
     {/* Container for item cards */}
     <div>
-
+      <h1>Cards go here</h1>
     </div>
   </div>
 
