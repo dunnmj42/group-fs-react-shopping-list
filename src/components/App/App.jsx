da59dc4c9a1d54163f1bb2c8d3f784ed98ce19f7
Connor Detviler <connor.detviler@gmail.com>
2021-01-14 10:15:39 -0600

    imported Form component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-import Header from '../Header/Header.jsx'
+import Header from '../Header/Header.jsx';
 import './App.css';
+import Form from '../Form/Form.jsx';
 
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <Form />
             <main>
                 <p>Under Construction...</p>
             </main>
