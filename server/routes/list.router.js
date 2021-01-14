c3f9592278c92fc79f8438067e33c03015367085
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2021-01-14 11:02:38 -0600

    Delete button properly communicates with the database.

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -5,4 +5,19 @@ const pool = require('../modules/pool.js');
 // TODO - Add routes here...
 
 
+
+// Delete
+router.delete('/:id', (req, res) => {
+    let id = req.params.id
+    console.log('Deleting item number', id);
+    
+    let queryText = `SELECT * FROM shopping_list
+                    WHERE "id" = $1`
+
+    pool.query(queryText, [id]).then((results) => {
+        console.log(results.rows);
+        res.sendStatus(204)
+    })
+})
+
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
-
+import axios from 'axios';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
 function App() {
+
+    const removeItem =()=> {
+        console.log('delete clicked');
+
+        let id = 1;
+        axios.delete(`/list/1`).then((response) => {
+            console.log(response);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
     return (
         <div className="App">
             <Header />
             <main>
                 <p>Under Construction...</p>
             </main>
+            <button onClick={removeItem}>Delete</button>
         </div>
     );
 }
