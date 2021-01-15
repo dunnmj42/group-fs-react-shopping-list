c9a18ed9e2feef6f59d272c210c8587289e3c469
Connor Detviler <connor.detviler@gmail.com>
2021-01-15 11:05:28 -0600

    Changed colors on a bunch of stuff

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import "./Header.css";
 
 function Header() {
   return (
-    <Jumbotron>
-      <h1>My Shopping List</h1>
+    <Jumbotron className={"header"}>
+      <h1 className={"jumbo-text"}>My Shopping List</h1>
     </Jumbotron>
   );
 }
